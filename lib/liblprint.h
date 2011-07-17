73077b36ab5e6f5134ffa80c49f9f5afa5b75934
Tobias Frei <shuntingyard@gmail.com>
2011-07-18 01:46:46 +0200

    clumsily rm'ed some files because I forgot to mv them

--- lib/liblprint.h
+++
@@ -1,7 +0,0 @@
-/*
- *    liblprprint.h:
- *    routines in liblprprint.a
- *    and liblprprint.so
- */
-
-extern void lpr_print (char *the_text);
--- src/iprange.c
+++
@@ -1,8 +0,0 @@
-#include "liblprint.h"
-
-/* To shorten example, not using argp */
-int main ()
-{
-    lpr_print ("Hello, Multiverse!\nHowarya?\n");
-    return 0;
-}
